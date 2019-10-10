6cd6f9c9f650fc69a1d7f5ad91a828bc1b6be25c
Trubnikov <trubnikovdv@mail.ru>
2019-10-11 01:59:03 +0300

    Change view for frontend

--- src/main/kotlin/legends/views/GhostView.kt
+++ src/main/kotlin/legends/views/GhostView.kt
@@ -5,7 +5,7 @@ import legends.models.GhostModel
 
 data class GhostView(
         @JsonProperty("ghost_id") val ghostId: Long,
-        @JsonProperty("history") val history: String
+        @JsonProperty("history") val history: String?
 ) {
     constructor(ghost: GhostModel) : this(
             ghostId = ghost.ghostId,
@@ -13,4 +13,12 @@ data class GhostView(
     )
 }
 
-fun List<GhostModel>.toView(): List<GhostView> = map { GhostView(it) }
+fun List<GhostModel>.toView(): List<GhostView> {
+    val list = ArrayList<GhostView>(6)
+    for (ghostIndex in 1..6L) {
+        val ghost = firstOrNull { it.ghostId == ghostIndex }
+        val ghostView = ghost?.let { GhostView(it) } ?: GhostView(ghostIndex, null)
+        list.add(ghostView)
+    }
+    return list;
+}
